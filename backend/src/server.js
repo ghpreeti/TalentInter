ccf643a420dd13f6fc32f1e3b7ef29a90a2ebb97
ghpreeti <preetisinghitt@gamil.com>
2025-12-05 01:34:37 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {ENV} from './lib/env.js';
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get('/', (req, res) => {
+  res.status(200).json({msg:'Hello, World!'});
+});
+
+app.listen(ENV.PORT, () => {
+  console.log('Server is running on port', ENV.PORT);
+});
