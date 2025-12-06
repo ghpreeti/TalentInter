8437a444bf7fa41462f7bb194b564ad3644778b2
ghpreeti <preetisinghitt@gamil.com>
2025-12-07 02:23:16 +0530

    importing functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,48 +1,55 @@
-import express from 'express';
-import path from 'path';
-import {ENV} from './lib/env.js';
-import {connectDB} from './lib/db.js';
-import cors from 'cors';
-import {serve} from 'inngest/express';
-import {inngest} from './lib/inngest.js';
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { serve} from "inngest/express";
+// import { clerkMiddleware } from "@clerk/express";
 
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
+
+// import chatRoutes from "./routes/chatRoutes.js";
+// import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-//middleware to parse JSON requests
+// middleware
 app.use(express.json());
-//creadentials:true?? => server allow browser to include cookies to be sent in cross-origin requests
-app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+// app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
-app.use("/api/inngest", serve({client:inngest,functions}))
+app.use("/api/inngest", serve({ client: inngest, functions}));
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/sessions", sessionRoutes);
 
-app.get('/health', (req, res) => {
-  res.status(200).json({msg:'Hello, World!'});
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
 });
 
-// make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.get("/", (req, res) => {
+  res.send("Backend is live");
+});
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () => {
-  console.log('Server is running on port', ENV.PORT);
-  connectDB();
-});
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
-    console.error('Failed to start server:', error);
+    console.error("💥 Error starting the server", error);
   }
-}
+};
 
 startServer();
-
